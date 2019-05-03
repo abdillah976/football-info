fad809b7f4e576ba50af59f2c25e02260fe4fe42
Abdillah MOHAMED OMAR <abdillah.mohamed@hotmail.fr>
2019-05-03 18:21:59 +0200

    Gestion des joueurs

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const https = require('https');
 
 const url = 'https://fr.uefa.com/uefachampionsleague/season=2019/clubs/';
 const pathClubsFile = './parse/clubs.html';
+const pathTeamsFile = './parse/teams';
+const logoFolder = './public/logo';
+
+var teamsInfo = [];
 
 function getClubFile() {
     https.get(url, (res) => {
@@ -14,13 +18,22 @@ function getClubFile() {
     });
 }
 
+function getTeamFile(teamId, teamUrl) {
+    console.log(teamUrl);
+    https.get(teamUrl, (res) => {
+        res.pipe(fs.createWriteStream(`${pathTeamsFile}/${teamId}.html`));
+        res.on('end', () => {
+            console.log(`Fichier ${teamId} enregistré`);
+        });
+    });
+}
+
 function parseFile(path) {
     fs.readFile(path, (err, file) => {
         if(err)console.log(err);
         const $ = cheerio.load(file.toString());
         
         var teams = $('div.team-is-club');
-        var teamsInfo = [];
         
         teams.toArray().forEach(team => {
             // Récupération du nom de l'équipe
@@ -37,10 +50,9 @@ function parseFile(path) {
 
             
             var logoUrl = $(team).find('.club-logo').attr('style').split('(')[1].split("'")[1];
-            console.log(logoUrl);
             teamsInfo.push({'id':id, 'name':name, 'country':country, 'logoUrl':logoUrl});
             
-            const writeStream = fs.createWriteStream(`./public/logo/${id}.png`);
+            const writeStream = fs.createWriteStream(`${logoFolder}/${id}.png`);
             // Enregistre l'image dans le dossier public
             https.get(logoUrl, res => {
                 res.pipe(writeStream);
@@ -49,9 +61,33 @@ function parseFile(path) {
                     console.log('Fin du chargement');
                 })
             })
+
+            // Récupération des équipes
+            // `${url}/club=${teamId}`
+            // getTeamFile(teamId, `${url}/club=7889`);
+            
         });
     });
 }
 
+fs.readFile(pathTeamsFile+'/7889.html', (err, file) => {
+    if(err)console.log(err);
+    const $ = cheerio.load(file.toString());
+    var playerRole = null;
+    var playerNum = null;
+    var playerName = null;
+    var playerSurname = null;
+    
+    var players = $('li.squad--team-player');
+    players.toArray().forEach(player => {
+        playerRole = $(player).find('span.squad--player-role').text();
+        playerNum = $(player).find('span.squad--player-num').text();
+        playerName = $(player).find('span.squad--player-name a').text();
+        playerSurname = $(player).find('span.squad--player-name-surname a').text();
+    });
+    
+});
+
+// getTeamFile(7889, `${url}club=7889/`);
 // getClubFile();
 parseFile(pathClubsFile);
